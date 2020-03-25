53b1345cce1af588a477201c64f71d561cdb6f94
Domenico Guarino <domenico.guarino@gmail.com>
2020-03-25 09:25:52 +0100

    Update execute.bash

--- execute.bash
+++ execute.bash
@@ -1,14 +1,3 @@
-# Stability search for spindles
+# Example code run
 
-# rm -R SearchSpindles
-
-# python run.py --folder SearchSpindles --params params_Th.py --search search.py --map yes nest
-# python run.py --folder SearchTh2 --params params_Th.py --search search2.py --map yes nest
-
-
-python run.py --folder SearchPY_EPSP --params PY_epsp_response.py --search search.py --map yes nest
-python run.py --folder SearchINH_EPSP --params INH_epsp_response.py --search search.py --map yes nest
-python run.py --folder SearchPY_IPSP --params PY_ipsp_response.py --search search.py --map yes nest
-python run.py --folder SearchINH_IPSP --params INH_ipsp_response.py --search search.py --map yes nest
-
-# python plot_map.py
+python run.py --folder SW_Delta --params thalamocortical_delta.py nest
